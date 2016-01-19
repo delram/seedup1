af2f006b5c2205a42e33a9cf1d49c45aebc10d8b
delram <zari.z937@gmail.com>
2016-01-19 19:52:43 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -219,6 +219,7 @@ function create_config( )
     "download_media",
     "invite",
     "all",
+    "plugins",
     "leave_ban"
     },
     sudo_users = {94477327,144976651,173666523,140503292,170774776},--Sudo users
